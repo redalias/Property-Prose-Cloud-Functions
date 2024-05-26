1da7247f70e71f18691848fdc2a8db9d1588ac1a
Russel Abarintos <rabarint@gmail.com>
2024-05-26 14:18:15 +1200

    Add stripe-events.js

---
+++ functions/values/stripe-events.js
@@ -0,0 +1,7 @@
+const stripeEvents = {
+  checkoutSessionCompleted: "checkout.session.completed",
+  customerSubscriptionDeleted: "customer.subscription.deleted",
+  customerSubscriptionUpdated: "customer.subscription.updated",
+};
+
+module.exports = stripeEvents;
