64a8720764122ddb82b0acb18e78837390188efa
Russel Abarintos <rabarint@gmail.com>
2025-02-07 10:51:45 +1300

    Strings: add copy element types

--- functions/values/strings.js
+++ functions/values/strings.js
@@ -2,6 +2,22 @@ const strings = {
   subscriptionStatusFree: 'Free',
   subscriptionStatusPro: 'Pro',
   subscriptionStatusProPendingDowngrade: 'Pro (pending downgrade)',
+
+  listingTitle: 'Listing title',
+  listingBody: 'Listing body',
+  propertyListing: 'Property Listing',
+  title: 'Title',
+  prospectiveEmail: 'Prospective Email',
+  prospectiveEmailSubject: 'Prospective email subject',
+  prospectiveEmailBody: 'Prospective email body',
+  subject: 'Subject',
+  body: 'Body',
+  socialMedia: 'Social Media',
+  facebookPost: 'Facebook Post',
+  instagramPost: 'Instagram Post',
+  xPost: 'X Post',
+  linkedInPost: 'LinkedIn Post',
+  videoTour: 'Video Tour',
 };
 
 module.exports = strings;
